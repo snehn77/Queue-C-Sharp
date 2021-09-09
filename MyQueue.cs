62fc57bff8b9bb4cf9d2743f938a483051c11a17
Sneh Nandu <snehn777@gmail.com>
2021-09-09 13:08:36 -0700

    Add files via upload

---
+++ MyQueue.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Queue
+{
+    class MyQueue
+    {
+        private int QueueSize;
+
+        public int front = 0,rear = 0;
+
+        // Implementing queue using Array
+        Object[] queue;
+
+        // Default constructor is called taking in the size of queue as parameter
+        public MyQueue(int SizeOfQueue)
+        {
+            queue = new Object[SizeOfQueue];
+            QueueSize = SizeOfQueue;
+        }
+
+
+        public void Enqueue(object element)
+        {
+            // Condition for if Queue is full
+            if (QueueSize == Size())
+            {
+                Console.WriteLine("Queue is full");
+            }
+            // If not full insert
+            else
+            {
+                queue[rear++] = element;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nOperation Successful");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        public void Dequeue()
+        {
+            int size = Size();
+            // Check if queue is empty
+            if (size == 0)
+            {
+                Console.WriteLine("Queue is empty");       
+            }
+            else if(size == 1)
+            {
+                queue[0] = null;
+                rear = 0;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nOperation Successful");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+            else
+            {
+                queue[front] = null;
+                for(int i = 0; i < size-1; i++)
+                {
+                    queue[i] = queue[i + 1];
+                }
+                queue[size - 1] = null;
+                rear = rear - 1;
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("\nOperation Successful");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+        }
+
+        public void Peek()
+        {
+            // Check if queue is empty
+            if (Size() == 0)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            // If not empty return the top element
+            else
+            {
+                Console.WriteLine("\nElement at Front: " + queue[front]);
+            }
+        }
+
+        public int Size()
+        {
+            int count = 0;
+            // Check if queue is empty
+            foreach (var i in queue)
+            {
+                if (i != null)
+                {
+                    count = count + 1;
+                }
+            }
+            return count;
+        }
+
+        public bool Contains(object element)
+        {
+            Console.WriteLine();
+            // Check if queue is empty
+            int size = Size();
+            if (size == 0)
+            {
+                return false;
+            }
+            // If element in stack return true
+            for (int i=0 ; i<size ; i++)
+            {
+                if(queue[i].Equals(element))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public void Reverse()
+        {
+            // Check if queue is empty
+            int size = Size();
+            if (size == 0)
+            {
+                Console.WriteLine("Queue is empty");
+                return;
+            }
+
+            object[] reverseQueue = new Object[size];
+            int count = 0;
+
+            for(int i = rear-1; i >= front; i--)
+            {
+                reverseQueue[count] = queue[i];
+                count = count + 1;
+            }
+
+            for (int i = 0; i< size; i++)
+            {
+                queue[i] = reverseQueue[i];
+            }
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("\nQueue Reversed Successfully!");
+            Console.ForegroundColor = ConsoleColor.White;
+        }
+
+        // This method gives Iterated List form the starting index of element using IEnumerable and yeild return
+        public void Iterator()
+        {
+
+            // Check if queue is empty
+            if (Size() == 0)
+            {
+                Console.WriteLine("Queue is Empty");
+                return;
+            }
+
+
+            IEnumerable<object> myQueue = GetIteratedQueue();
+            Console.WriteLine("\nIterated Queue is: ");
+            foreach (var i in myQueue)
+            {
+                if (i != null)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+
+        public IEnumerable<object> GetIteratedQueue()
+        {
+            foreach (var items in queue)
+            {
+                // Returning the element after every iteration
+                yield return items;
+            }
+        }
+
+        public void Print()
+        {
+            Console.WriteLine();
+            if (Size() == 0)
+            {
+                Console.WriteLine("Queue is Empty");
+                return;
+            }
+            for (int i = 0; i < Size(); i++)
+            {
+                Console.WriteLine("Item {0}: {1}", (i+1), queue[i]);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Declaring a queue of size 10. (Note - You can change the size of stack as per your need)
+            // For this question we Have taken a queue that takes in only integers
+            MyQueue myQueue = new MyQueue(10);
+            int flag = 0, element;
+
+        redo:
+            Console.Clear();
+            ShowOptions();
+            int choice = Choice();
+            while (flag == 0)
+            {
+                switch (choice)
+                {
+                    case 1:
+                        element = GetValue();
+                        myQueue.Enqueue(element);
+                        PressToContinue();
+                        goto redo;
+
+                    case 2:
+                        myQueue.Dequeue();
+                        PressToContinue();
+                        goto redo;
+
+                    case 3:
+                        myQueue.Peek();
+                        PressToContinue();
+                        goto redo;
+
+                    case 4:
+                        element = GetValue();
+                        Console.WriteLine(myQueue.Contains(element));
+                        PressToContinue();
+                        goto redo;
+
+                    case 5:
+                        Console.WriteLine("\nSize is: " + myQueue.Size());
+                        PressToContinue();
+                        goto redo;
+
+                    case 6:
+                        myQueue.Reverse();
+                        PressToContinue();
+                        goto redo;
+
+                    case 7:
+                        myQueue.Iterator();
+                        PressToContinue();
+                        goto redo;
+
+                    case 8:
+
+                        myQueue.Print();
+                        PressToContinue();
+                        goto redo;
+                    // Case to exit the application
+                    case 0:
+                        flag = 1;
+                        break;
+                }
+            }
+        }
+        // Displaying operations
+        public static void ShowOptions()
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("Press 0 ---> Exit");
+            Console.WriteLine("Press 1 ---> Enqueue");
+            Console.WriteLine("Press 2 ---> Dequeue");
+            Console.WriteLine("Press 3 ---> Peek");
+            Console.WriteLine("Press 4 ---> Contains");
+            Console.WriteLine("Press 5 ---> Size");
+            Console.WriteLine("Press 6 ---> Reverse");
+            Console.WriteLine("Press 7 ---> Iterator");
+            Console.WriteLine("Press 8 ---> Print");
+        }
+        // To get choice of operation
+        public static int Choice()
+        {
+        redo:
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Write("\nEnter Choice of Operation: ");
+            int choice;
+            try
+            {
+                choice = int.Parse(Console.ReadLine());
+                while (choice < 0 || choice > 8)
+                {
+                    Console.Write("Please enter valid choice: ");
+                    choice = int.Parse(Console.ReadLine());
+                }
+                return choice;
+            }
+            catch (FormatException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nInvalid Type. Please enter a choice");
+                Console.ForegroundColor = ConsoleColor.White;
+                goto redo;
+            }
+        }
+        // To get value
+        public static int GetValue()
+        {
+        redo:
+            try
+            {
+                Console.Write("\nEnter Value : ");
+                int value = int.Parse(Console.ReadLine());
+                return value;
+            }
+            catch (FormatException)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("\nInvalid Type. Please enter a integer");
+                Console.ForegroundColor = ConsoleColor.White;
+                goto redo;
+            }
+        }
+
+        public static void PressToContinue()
+        {
+            Console.WriteLine("\nPress Any Key to continue: ");
+            Console.ReadKey();
+        }
+    }
+}
